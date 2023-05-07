05c1ba7a7191fa709728c599dda2a88208eecfaf
ShreyasJ1 <109956165+ShreyasJ1@users.noreply.github.com>
2023-05-07 14:51:15 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
+nose_x = 0;
+nose_y = 0;
+
+
 function preload() {
+img = loadImage('https://i.postimg.cc/7ZBcjDqp/clownnose.png');
 
 }
 
@@ -11,16 +16,19 @@ function setup() {
     video.hide();
 
     poseNet = ml5.poseNet(video, modelLoaded);
-    poseNet.on('pose',gotPoses);
+    poseNet.on('pose', gotPoses);
 }
 
 function gotPoses(results) {
-if(results.length > 0){
-console.log(results);
-console.log("nose x = "+ results[0].pose.nose.x);
-console.log("nose y = "+ results[0].pose.nose.y);
+    if (results.length > 0) {
+        console.log(results);
+        nose_x = results[0].pose.nose.x-10;
+        nose_y = results[0].pose.nose.y-10;
 
-}
+        console.log("nose x = " + nose_x);
+        console.log("nose y = " + nose_y);
+
+    }
 }
 
 function modelLoaded() {
@@ -29,6 +37,8 @@ function modelLoaded() {
 
 function draw() {
     image(video, 0, 0, 300, 300);
+    image(img,nose_x,nose_y,30,30);
+
 }
 
 function take_snapshot() {
